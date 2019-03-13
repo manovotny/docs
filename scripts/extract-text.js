e33523227352a446bd1e7faa2b66b3cfbddad13b
Michael Novotny <manovotny@gmail.com>
2019-03-13 15:52:46 -0500

    Breaking page content into sections.

--- scripts/extract-text.js
+++ scripts/extract-text.js
@@ -4,11 +4,28 @@ const globby = require('globby')
 
 ;(async () => {
   const files = await globby([
-    'out/guides/custom-next-js-server-to-routes/index.html'
+    '/Users/mnovotny/Developer/docs/out/docs/v1/continuous-integration/travis/index.html'
   ])
   files.forEach(file => {
     const content = fs.readFileSync(file)
     const $ = cheerio.load(content)
-    console.log($.text())
+    const headings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+    const structure = []
+
+    $('main aside')
+      .next()
+      .find(`${headings.join(', ')}, p`)
+      .each((index, element) => {
+        if (headings.includes(element.tagName)) {
+          structure.push({
+            section: $(element).text(),
+            content: []
+          })
+        } else {
+          structure[structure.length - 1].content.push($(element).text())
+        }
+      })
+
+    console.log(structure)
   })
 })()
